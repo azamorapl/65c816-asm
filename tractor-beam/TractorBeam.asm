64f9b79240a85fc15c7d41df9974cb197d79855a
Adolfo Zamora <adolfo.zamorapl@gmail.com>
2016-11-27 23:50:06 -0500

    Distance-based tractor beam

--- tractor-beam/TractorBeam.asm
+++ tractor-beam/TractorBeam.asm
@@ -1,34 +1,57 @@
 lorom
 
+;org $90B860 : DB $3C	;how long charge beam must be held MoveLeft to fire a beam (default 3C)	
+;org $91D756 : DB $3C	;charge beam timer check for samus palette to change (should be equal to above value, default 3C)
 
-org $90B860		;how long charge beam must be held down to fire a beam
+!SamusX = $0AF6
+!SamusY = $0AFA
+!DropX = $1A4B
+!DropY = $1A93
 
-	DB $3C		;$3C
+!MinDistance = #$0096
 
-org $91D756		;charge beam timer check for samus palette to change
+org $86EFE5 : JSR AttractDrops
+org $86F057 : LDA $0AF6 ;in case you had Black Falcon's patch
 
-	DB $3C
-
-org $86F057		;gets rid of the old hijack point
-
-	LDA $0AF6
-
-org $86EFE5 : JSR CHARGE
-
-org $86F4AB
-CHARGE:
-	LDA $0CD0 : BEQ GTFO
-	LDA $0AF6 : CMP $1A4B,x : BEQ NEXT : BMI UP : BPL DOWN : BRA GTFO
-
-NEXT:
-	LDA $1A93,x : CMP $0AFA : BEQ GTFO : BMI LEFT : BPL RIGHT
-UP:
-	DEC $1A4B,x : BRA NEXT
-DOWN:
-	INC $1A4B,x : BRA NEXT
-LEFT:
-	INC $1A93,x : BRA GTFO
-RIGHT:
-	DEC $1A93,x
-GTFO:
-	LDA $1B23,x : RTS
+org $86F600
+	AttractDrops:
+		LDA $0B62 : BEQ Exit
+		TestX:
+			LDA !SamusX : SEC : SBC !DropX,x : BEQ TestY : BMI +
+			CMP !MinDistance : BPL Exit : BRA MoveLeft
+		+	EOR #$FFFF : CMP !MinDistance : BPL Exit
+		MoveRight:
+			JSR CalculateXVelocity
+			EOR #$FFFF : CLC : ADC !DropX,x : STA !DropX,x : BRA TestY
+		MoveLeft:
+			JSR CalculateXVelocity
+			CLC : ADC !DropX,x : STA !DropX,x
+		TestY:
+			LDA !SamusY : SEC : SBC !DropY,x : BEQ Exit : BMI +
+			CMP !MinDistance : BPL Exit : BRA MoveDown
+		+	EOR #$FFFF : CMP !MinDistance : BPL Exit
+		MoveUp:
+			JSR CalculateYVelocity
+			EOR #$FFFF : CLC : ADC !DropY,x : STA !DropY,x : BRA Exit
+		MoveDown:
+			JSR CalculateYVelocity
+			CLC : ADC !DropY,x : STA !DropY,x
+		Exit:
+			LDA $1B23,X ;moved
+			RTS
+		CalculateXVelocity:
+			CMP #$0055 : BPL +
+			LDA #$0003 : BRA ++
+		+	LDA #$0002
+		++	RTS
+		CalculateYVelocity:
+			CMP #$0055 : BPL +
+			LDA #$0002 : BRA ++
+		+	LDA #$0001
+		++	RTS
+		VerticalSpeeds:
+			DW $0002,$0001
+		HorizontalSpeeds:
+			DW $0003,$0002
+		DistanceThresholds:
+			DW $0055,$0096
